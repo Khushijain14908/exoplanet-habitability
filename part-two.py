f8ad9436a4ddf9371b89a0e8f1429e42205d54f8
Khushijain14908 <khushijain14908@gmail.com>
2026-06-12 22:46:29 -0700

    completed online tutorial (analyzing student score data, graphing, etc.)

--- part-two.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt 
-
-#needed to do pip install ___ and select python interpreter based on where installed (I made venv)
-
-# to integrate into website, use Streamlit. use user input to generate graphs/visualizations. 
